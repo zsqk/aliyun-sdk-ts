b40e7b55ce35aeb4658861ef2c029cc1dccbe654
iugo <iugogogo@gmail.com>
2024-10-28 11:44:22 +0800

    rf: 移除历史代码

--- src/sls/test.ts
+++ src/sls/test.ts
@@ -1,23 +1,4 @@
 import { getLogs } from './get-logs.ts';
-import { queryLog } from './mod.ts';
-
-Deno.test('queryLog', async () => {
-  const res = await queryLog(
-    {
-      projectName: 'test',
-      logstoreName: 'test',
-      from: 1727664000,
-      to: 1727667600,
-    },
-    {
-      accessKeyId: '',
-      secretAccessKey: '',
-      endpoint: '',
-      apiVersion: '2015-06-01',
-    },
-  );
-  console.log(res);
-});
 
 const project = Deno.env.get('TEST_SLS_PROJECT')!;
 const logstore = Deno.env.get('TEST_SLS_LOGSTORE')!;
