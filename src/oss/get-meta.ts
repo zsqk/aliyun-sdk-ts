8b3ea797de8ddc802dbe0c3a5fe8d38b0b1798b7
iugo <iugogogo@gmail.com>
2025-09-29 16:47:56 +0800

    feat: Update HeadObjectResponse to include contentMd5 and storageClass fields with improved documentation

--- src/oss/get-meta.ts
+++ src/oss/get-meta.ts
@@ -109,23 +109,28 @@ export async function getObjectMeta({ bucket, path }: {
 
 type HeadObjectResponse = {
   /**
-   * Object通过生命周期规则转储为冷归档或者深度冷归档存储类型的时间。
+   * 对于Normal类型的Object，根据RFC 1864标准对消息内容（不包括Header）
+   * 计算Md5值获得128比特位数字，对该数字进行Base64编码作为一个消息的Content-Md5值。
+   * Multipart和Appendable类型的文件不会返回这个Header。
    */
-  transitionTime: string | null;
+  contentMd5: string | null;
   /**
-   * Object的最后一次访问时间。时间格式为HTTP 1.1协议中规定的GMT时间。
-   * 开启访问跟踪后，该字段的值会随着文件被访问的时间持续更新。
-   * 如果关闭了访问跟踪，则不再返回该字段。
+   * 表示Object的存储类型，分别为
+   *
+   * - 标准存储类型（Standard）
+   * - 低频访问存储类型（IA）
+   * - 归档存储类型（Archive）
+   * - 冷归档存储类型（ColdArchive）
+   * - 深度冷归档存储类型（DeepColdArchive）
+   *
+   * 更多信息，请参见
+   * {@link https://help.aliyun.com/zh/oss/overview-53/ 存储类型介绍}
    */
-  lastAccessTime: string | null;
+  storageClass: string | null;
   /**
    * Object最后一次修改时间。时间格式为HTTP 1.1协议中规定的GMT时间。
    */
   lastModified: string | null;
-  /**
-   * Object的版本ID。只有查看Object指定版本的元数据信息时才显示该字段。
-   */
-  versionId: string | null;
   /**
    * 表示该Object的64位CRC值。该64位CRC根据CRC64/XZ计算得出。
    * 对OSS支持CRC64校验前创建的Object，调用HeadObject接口时可能不会返回此响应头。
@@ -190,26 +195,26 @@ export async function headObject({ bucket, path }: {
 
   const h = new Headers(res.headers);
 
-  // 文档中写明的字段
+  // 通用字段
   const etag = h.get('ETag');
-  const transitionTime = h.get('x-oss-transition-time');
-  const lastAccessTime = h.get('x-oss-last-access-time');
+  const contentLength = h.get('Content-Length');
+
+  // 文档中写明的字段
+  const contentMd5 = h.get('Content-MD5');
   const lastModified = h.get('Last-Modified');
-  const versionId = h.get('x-oss-version-id');
   const crc64ecma = h.get('x-oss-hash-crc64ecma');
+  const storageClass = h.get('x-oss-storage-class');
 
   // 实测存在但文档中不明确的字段
-  const contentLength = h.get('Content-Length');
   const contentType = h.get('Content-Type');
 
   return {
     contentLength,
     etag,
-    transitionTime,
-    lastAccessTime,
     lastModified,
-    versionId,
     crc64ecma,
     contentType,
+    contentMd5,
+    storageClass,
   };
 }
