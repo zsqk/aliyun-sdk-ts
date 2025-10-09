e144a9a68e75759c354a161634a5dfdf8b1b0c9f
iugo <iugogogo@gmail.com>
2025-10-09 17:28:52 +0800

    test: Update @std/assert version and enhance crc64 test for improved validation

--- src/test/crc64.test.ts
+++ src/test/crc64.test.ts
@@ -1,6 +1,8 @@
-Deno.test('npm:crc64-ecma182.js', async () => {
+import { assertEquals } from '@std/assert';
+
+Deno.test('jsr:@zsqk/crc64', async () => {
   const { crc64 } = await import('@zsqk/crc64');
   const data = new TextEncoder().encode('Hello, world!\n');
   const hash = crc64(data);
-  console.log(hash);
+  assertEquals(hash, '14559282277039517123');
 });
